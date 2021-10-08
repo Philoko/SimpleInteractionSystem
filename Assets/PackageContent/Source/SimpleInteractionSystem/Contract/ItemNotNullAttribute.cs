1e8dc0a48b8d33316bf765437d62228ff75336b7
Philipp Okoampah <p.okoampah@gmx.de>
2021-10-08 15:00:36 +0200

    [RMOVED] 'ItemNotNullAttribute' because it is existent anway in newer versions of Unity

--- Assets/PackageContent/Source/SimpleInteractionSystem/Contract/ItemNotNullAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SimpleInteractionSystem.Contract
-{
-    [AttributeUsage(
-        AttributeTargets.Method | AttributeTargets.Parameter | AttributeTargets.Property |
-        AttributeTargets.Delegate | AttributeTargets.Field)]
-    public sealed class ItemNotNullAttribute : Attribute { }
-}
